72966945f33a27500d11d029ce761bde6243a92d
KrisCampbell <kriscampbell1997@gmail.com>
2020-05-28 15:45:46 -0700

    pre-heroku

--- think.py
+++ think.py
@@ -1,43 +1,51 @@
 import json
 import random
 
+def checknb(snakes,position):
+	leftpos = position.copy()['x'] -= 1
+	rightpos = position.copy()['x'] += 1
+	uppos = position.copy()['y'] += 1
+	downpos = position.copy()['y'] -= 1
+	neighborsOpen = 4
+	for snake in snakes:
+		if leftpos in snake['body']:
+			neighborsOpen -= 1
+		if rightpos in snake['body']:
+			neighborsOpen -= 1
+		if uppos in snake['body']:
+			neighborsOpen -= 1
+		if downpos in snake['body']:
+			neighborsOpen -= 1
+
+	return neighborsOpen
+
+
 def think(data,possible_moves):
 	me = data['you']['body']
 	myHead = data['you']['head']
 	myID = data['you']['id']
 	opponent = data['board']['snakes']
+	snakes = data['board']['snakes']
 	for snake in opponent:
 		if snake['id'] == myID:
 			opponent.remove(snake)
 	opponent = opponent[0]
 	oppBody = opponent['body']
 	headCopy = myHead.copy()
-	headCopy['x'] += 1
-	left = False
-	right = False
-	up = False
-	down = False
-	if headCopy not in me and headCopy['x'] < 11 and headCopy not in oppBody:
-		right = True
-
-	headCopy['x'] -= 2
-	if headCopy not in me and headCopy['x'] >= 0 and headCopy not in oppBody:
-		left = True
-
-	headCopy['x'] += 1
-	headCopy['y'] += 1
-	if headCopy not in me and headCopy['y'] < 11 and headCopy not in oppBody:
-		up = True
-
-	headCopy['y'] -= 2
-	if headCopy not in me and headCopy['y'] >= 0 and headCopy not in oppBody:
-		down = True
-
-	options = []
-	if left: options.append("left")
-	if right: options.append("right")
-	if up: options.append("up")
-	if down: options.append("down")
-	choice = random.choice(options)
-
-	return choice
+	leftpos = headCopy.copy()['x'] -= 1
+	rightpos = headCopy.copy()['x'] += 1
+	uppos = headCopy.copy()['y'] += 1
+	downpos = headCopy.copy()['y'] -= 1
+	leftCheck = checknb(snakes,leftpos)
+	rightCheck = checknb(snakes,rightpos)
+	upCheck = checknb(snakes,up)
+	downCheck = checknb(snakes,down)
+	maxCheck = max(leftCheck,rightCheck,upCheck,downCheck)
+	if(maxCheck == leftCheck):
+		return 'left'
+	elif(maxCheck == upCheck):
+		return 'up'
+	elif(maxCheck == rightCheck):
+		return 'right'
+	elif(maxCheck == downCheck):
+		return 'down'
