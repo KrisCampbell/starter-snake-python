d0be0f872fb564f24535c7348da5f0483aa72923
KrisCampbell <kriscampbell1997@gmail.com>
2020-05-28 13:32:31 -0700

    pre-heroku

--- think.py
+++ think.py
@@ -1,25 +1,13 @@
 import json
 import random
 
-def think(data,possibleMoves):
-	lastMove = "None"
-	with open('lastmove.json') as json_file:
-		data = json.load(json_file)
-		lastMove = data['move']
-
-	if lastMove == "None":
-		pass
-	elif lastMove == "up":
-		lastMove = "down"
-	elif lastMove == "down":
-		lastMove = "up"
-	elif lastMove == "left":
-		lastMove = "right"
-	elif lastMove == "right":
-		lastMove = "left"
-	copy = possibleMoves.copy()
-	if lastMove in copy: copy.remove(lastMove)
-	choice = random.choice(copy)
+def think(data):
+	choice = random.choice(data)
+	print(data)
+	# me = data['snakes']['you']['body']
+	# myHead = data['snakes']['you']['head']
+	# possibleMoves = [myHead.copy()['x']+=1,myHead.copy()['x']-=1,myHead.copy()['y']+=1,myHead.copy()['y']-=1]
+	# print("Possible:",possibleMoves)
 
 	with open('lastmove.json','w') as json_file:
 		json.dump({"move":choice},json_file)
