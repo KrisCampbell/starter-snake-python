56174baf899fc73f8dcd86a3071babcebd048edd
KrisCampbell <kriscampbell1997@gmail.com>
2020-05-28 13:53:12 -0700

    pre-heroku

--- think.py
+++ think.py
@@ -3,13 +3,25 @@ import random
 
 def think(data,possible_moves):
 	choice = random.choice(possible_moves)
-	print(data)
-	# me = data['snakes']['you']['body']
-	# myHead = data['snakes']['you']['head']
-	# possibleMoves = [myHead.copy()['x']+=1,myHead.copy()['x']-=1,myHead.copy()['y']+=1,myHead.copy()['y']-=1]
-	# print("Possible:",possibleMoves)
 
-	with open('lastmove.json','w') as json_file:
-		json.dump({"move":choice},json_file)
+	me = data['you']['body']
+	myHead = data['you']['head']
+	headCopy = myHead.copy()
+	headCopy['x'] += 1
+	if headCopy not in me and headCopy['x'] < 11:
+		return 'right'
 
-	return choice
+	headCopy['x'] -= 2
+	elif headCopy not in me and headCopy['x'] >= 0:
+		return 'left'
+
+	headCopy['x'] += 1
+	headCopy['y'] += 1
+	elif headCopy not in me and headCopy['y'] < 11:
+		return 'up'
+
+	headCopy['y'] -= 2
+	elif headCopy not in me and headCopy['y'] >= 0:
+		return 'down'
+
+	return 'up'
