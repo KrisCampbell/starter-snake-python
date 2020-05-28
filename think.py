060fe40a66720b6fbe016601324fa4d9233fa32b
KrisCampbell <kriscampbell1997@gmail.com>
2020-05-28 14:04:10 -0700

    pre-heroku

--- think.py
+++ think.py
@@ -6,22 +6,28 @@ def think(data,possible_moves):
 
 	me = data['you']['body']
 	myHead = data['you']['head']
+	opponent = data['snakes']
+	for snake in opponent:
+		if snake['id'] == 'gs_hcTjRDfFPjtD4Qxmg3TYtxDK':
+			opponent.remove(snake)
+	opponent = opponent[0]
+	oppBody = opponent['body']
 	headCopy = myHead.copy()
 	headCopy['x'] += 1
-	if headCopy not in me and headCopy['x'] < 11:
+	if headCopy not in me and headCopy['x'] < 11 and headCopy not in oppBody:
 		return 'right'
 
 	headCopy['x'] -= 2
-	if headCopy not in me and headCopy['x'] >= 0:
+	if headCopy not in me and headCopy['x'] >= 0 and headCopy not in oppBody:
 		return 'left'
 
 	headCopy['x'] += 1
 	headCopy['y'] += 1
-	if headCopy not in me and headCopy['y'] < 11:
+	if headCopy not in me and headCopy['y'] < 11 and headCopy not in oppBody:
 		return 'up'
 
 	headCopy['y'] -= 2
-	if headCopy not in me and headCopy['y'] >= 0:
+	if headCopy not in me and headCopy['y'] >= 0 and headCopy not in oppBody:
 		return 'down'
 
 	return 'up'
