c8cbd7a2f8c7c5016c2ebbe0e2d9e0a6b67ec538
KrisCampbell <kriscampbell1997@gmail.com>
2020-05-28 13:04:55 -0700

    pre-heroku

--- think.py
+++ think.py
@@ -7,7 +7,21 @@ def think(data,possibleMoves):
 		data = json.load(json_file)
 		lastMove = data['move']
 
+	if lastMove == "None":
+		pass()
+	elif lastMove == "Up":
+		lastMove = "Down"
+	elif lastMove == "Down":
+		lastMove == "Up"
+	elif lastMove == "Left":
+		lastMove = "Right"
+	elif lastMove == "Right":
+		lastMove == "Left"
 	copy = possibleMoves.copy()
 	if lastMove in copy: copy.remove(lastMove)
 	choice = random.choice(copy)
+
+	with open('lastmove.json','w') as json_file:
+		json.dump({"move":choice},json_file)
+
 	return choice
